757a9189fd4b022f81b0833d118e78520aa78838
king danie <daniedemola@gmail.com>
2023-07-05 17:05:51 +0100

    first commit

---
+++ rankchart.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name:     Rankchart
+ * Plugin URI:      https://github.com/kingdanie
+ * Description:     Plugin that displays daily SEO charts on the Wordpress dashboard
+ * Author:          Danie D'mola
+ * Author URI:       https://github.com/kingdanie
+ * Text Domain:     rankchart
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Rankchart
+ */
+
+//if this file is accessed directly, abort!!
+define('ASBPATH') or die('Unauthorized Access');
+
+//define constants
+define('CR_PLUGIN-VERSION', '0.1.0');
+define('CR_PLUGIN_DIR', plugin_dir_url( _FILE__ ));
+
