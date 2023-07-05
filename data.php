de4dd857cb1ab548f87917c7c633268917a9d9a2
king danie <daniedemola@gmail.com>
2023-07-06 00:29:45 +0100

    minor adjustments

--- data.php
+++ data.php
@@ -1,137 +1,141 @@
 <?php
-
 /**
- * @file
- * Provides the data that is being fed into the database table when the plugin is activated.
+ * Fired during plugin activation
+ *
+ * @link       github.com/kingdanie
+ * @since      0.1.0
+ *
+ * @package    Rank_Chart
  */
+
 
 header( 'Content-Type => application/json' );
 
 $data = array(
-  [
-    'day' => '1',
-    'setScore' => 4000,
-  ],
-  [
-    'day' => '2',
-    'setScore' => 3000,
-  ],
-  [
-    'day' => '3',
-    'setScore' => 2000,
-  ],
-  [
-    'day' => '4',
-    'setScore' => 2780,
-  ],
-  [
-    'day' => '5',
-    'setScore' => 1890,
-  ],
-  [
-    'day' => '6',
-    'setScore' => 2390,
-  ],
-  [
-    'day' => '7',
-    'setScore' => 3490,
-  ],
-  [
-    'day' => '8',
-    'setScore' => 2780,
-  ],
-  [
-    'day' => '9',
-    'setScore' => 1890,
-  ],
-  [
-    'day' => '10',
-    'setScore' => 2390,
-  ],
-  [
-    'day' => '11',
-    'setScore' => 3490,
-  ],
-  [
-    'day' => '12',
-    'setScore' => 2180,
-  ],
-  [
-    'day' => '13',
-    'setScore' => 5890,
-  ],
-  [
-    'day' => '14',
-    'setScore' => 390,
-  ],
-  [
-    'day' => '15',
-    'setScore' => 3490,
-  ],
-  [
-    'day' => '16',
-    'setScore' => 4000,
-  ],
-  [
-    'day' => '17',
-    'setScore' => 3000,
-  ],
-  [
-    'day' => '18',
-    'setScore' => 2000,
-  ],
-  [
-    'day' => '19',
-    'setScore' => 2780,
-  ],
-  [
-    'day' => '20',
-    'setScore' => 1890,
-  ],
-  [
-    'day' => '21',
-    'setScore' => 2390,
-  ],
-  [
-    'day' => '22',
-    'setScore' => 3490,
-  ],
-  [
-    'day' => '23',
-    'setScore' => 2780,
-  ],
-  [
-    'day' => '24',
-    'setScore' => 1890,
-  ],
-  [
-    'day' => '25',
-    'setScore' => 2390,
-  ],
-  [
-    'day' => '26',
-    'setScore' => 3490,
-  ],
-  [
-    'day' => '27',
-    'setScore' => 2180,
-  ],
-  [
-    'day' => '28',
-    'setScore' => 5890,
-  ],
-  [
-    'day' => '29',
-    'setScore' => 390,
-  ],
-  [
-    'day' => '30',
-    'setScore' => 3490,
-  ],
-  [
-    'day' => '31',
-    'setScore' => 4000,
-  ]
+        [
+          'day' => '1',
+          'setScore' => 4000,
+        ],
+        [
+          'day' => '2',
+          'setScore' => 3000,
+        ],
+        [
+          'day' => '3',
+          'setScore' => 2000,
+        ],
+        [
+          'day' => '4',
+          'setScore' => 2780,
+        ],
+        [
+          'day' => '5',
+          'setScore' => 1890,
+        ],
+        [
+          'day' => '6',
+          'setScore' => 2390,
+        ],
+        [
+          'day' => '7',
+          'setScore' => 3490,
+        ],
+        [
+          'day' => '8',
+          'setScore' => 2780,
+        ],
+        [
+          'day' => '9',
+          'setScore' => 1890,
+        ],
+        [
+          'day' => '10',
+          'setScore' => 2390,
+        ],
+        [
+          'day' => '11',
+          'setScore' => 3490,
+        ],
+        [
+          'day' => '12',
+          'setScore' => 2180,
+        ],
+        [
+          'day' => '13',
+          'setScore' => 5890,
+        ],
+        [
+          'day' => '14',
+          'setScore' => 390,
+        ],
+        [
+          'day' => '15',
+          'setScore' => 3490,
+        ],
+        [
+          'day' => '16',
+          'setScore' => 4000,
+        ],
+        [
+          'day' => '17',
+          'setScore' => 3000,
+        ],
+        [
+          'day' => '18',
+          'setScore' => 2000,
+        ],
+        [
+          'day' => '19',
+          'setScore' => 2780,
+        ],
+        [
+          'day' => '20',
+          'setScore' => 1890,
+        ],
+        [
+          'day' => '21',
+          'setScore' => 2390,
+        ],
+        [
+          'day' => '22',
+          'setScore' => 3490,
+        ],
+        [
+          'day' => '23',
+          'setScore' => 2780,
+        ],
+        [
+          'day' => '24',
+          'setScore' => 1890,
+        ],
+        [
+          'day' => '25',
+          'setScore' => 2390,
+        ],
+        [
+          'day' => '26',
+          'setScore' => 3490,
+        ],
+        [
+          'day' => '27',
+          'setScore' => 2180,
+        ],
+        [
+          'day' => '28',
+          'setScore' => 5890,
+        ],
+        [
+          'day' => '29',
+          'setScore' => 390,
+        ],
+        [
+          'day' => '30',
+          'setScore' => 3490,
+        ],
+        [
+          'day' => '31',
+          'setScore' => 4000,
+        ]
     );
 
 echo json_encode($data);
